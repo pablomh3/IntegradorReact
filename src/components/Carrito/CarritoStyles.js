29d459757e42d00c56850b1ce0ac0854c68df0fd
pablomh3 <pablonudenberg@gmail.com>
2023-06-19 03:51:17 -0300

    res

--- src/components/Carrito/CarritoStyles.js
+++ src/components/Carrito/CarritoStyles.js
@@ -83,8 +83,8 @@ export const CartContainer = styled.div`
         padding: 6px;
         flex-direction: column;
         img{
-            width: 40px;
-            height: 40px;
+            width: 50px;
+            height: 50px;
         }
 }
 `
