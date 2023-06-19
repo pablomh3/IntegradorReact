769c4ee2cc7abb95f5bc77b9395fc850ed26df54
pablomh3 <pablonudenberg@gmail.com>
2023-06-19 03:34:35 -0300

    res

--- src/pages/Main/MainStyles.js
+++ src/pages/Main/MainStyles.js
@@ -3,10 +3,12 @@ import styled from "styled-components";
 export const MainWrapper = styled.div`
   width: 90vw;
   max-width: 1600px;
-  padding: 2rem;
+  padding: 0rem;
   margin: 0 auto;
   
 @media (max-width: 576px){
-    padding: 1rem;
+    padding: 0;
+    max-width: 390px;
+    width: 100%;
 }
 `;
