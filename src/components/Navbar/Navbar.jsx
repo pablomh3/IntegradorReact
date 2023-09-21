d2513e3e5978e00be261ab7e1a31e398e41166df
pablomh3 <pablonudenberg@gmail.com>
2023-09-21 16:29:02 -0300

    back

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -41,7 +41,7 @@ const dispatch = useDispatch();
             <ContainerUserCarrito>
                 { currentUser ? 
                 <>
-                    <IniciaSesion onClick={() => navigate('/mis-ordenes')}> <ImUser size={22}/> <u> Tus órdenes </u> </IniciaSesion>
+                    <IniciaSesion onClick={() => navigate('/mis-ordenes')}> <ImUser size={22}/> <u> Mis órdenes </u> </IniciaSesion>
                     <Registrarse onClick={() => {dispatch(setCurrentUser(null))}}>  <u>Cerrar sesión </u></Registrarse>
                 </> :
                 <>
