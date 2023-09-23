fe596669bf32e82bcdce94c881ade7719d0941bb
pablomh3 <pablonudenberg@gmail.com>
2023-09-23 19:37:21 -0300

    santi

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const INITIAL_LIMIT = 8;
 export const SHIPPING = 1000;
-export const BASE_URL = 'https://integradorback.vercel.app'
+export const BASE_URL = 'https://backend-integrador.vercel.app'
 export const regEmail =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
