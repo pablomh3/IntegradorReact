dd4dea3427589edcb71969909f2ffe8f5906cb3e
pablomh3 <pablonudenberg@gmail.com>
2023-09-24 15:32:00 -0300

    date

--- src/pages/Ordenes/CardsOrdenes.jsx
+++ src/pages/Ordenes/CardsOrdenes.jsx
@@ -4,10 +4,12 @@ import { formatDate } from '../../constants/constants'
 
 const CardsOrdenes = ({ createdAt, total, _id }) => {
 
+  const formattedDate = formatDate(new Date(createdAt))
+
   return (
     <Orders> 
             <h3>ID de la Orden: #{_id.slice(0, 7)}</h3>
-            <p> <strong> Fecha: </strong> {formatDate({createdAt.slice(0, 10)})} </p>
+            <p><strong>Fecha: </strong>{formattedDate}</p>
             <h2> ${total}</h2>
     </Orders>
   )
