0777512293ed55aec64efcdcff89062344c71891
pablomh3 <pablonudenberg@gmail.com>
2023-09-24 15:22:26 -0300

    orders

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,3 +3,9 @@ export const SHIPPING = 1000;
 export const BASE_URL = 'https://integradorback.vercel.app'
 export const regEmail =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  export const formatDate = date =>
+  new Intl.DateTimeFormat('es-AR', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  }).format(date);
--- src/pages/Ordenes/CardsOrdenes.jsx
+++ src/pages/Ordenes/CardsOrdenes.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Orders } from './MisOrdenesStyles'
+import { formatDate } from '../../constants/constants'
 
 const CardsOrdenes = ({ createdAt, total, _id }) => {
 
   return (
     <Orders> 
             <h3>ID de la Orden: #{_id.slice(0, 7)}</h3>
-            <p> <strong> Fecha: </strong> {createdAt.slice(0, 10)}</p>
+            <p> <strong> Fecha: </strong> {formatDate({createdAt})}</p>
             <h2> ${total}</h2>
     </Orders>
   )
